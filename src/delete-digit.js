1e0900f72c5822a21c8146dbfdcf020c15d36d6a
Marharyta <rita.shyshkavets@gmail.com>
2025-01-04 19:06:00 +0100

    complete 3 more tasks

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const newNumber = Array.from(String(n));
+  let maxNumber = 0;
+  for (let i = 0; i < newNumber.length; i += 1) {
+    const leftWithoutCurrent = [
+      ...newNumber.slice(0, i),
+      ...newNumber.slice(i + 1)
+    ];
+    const withoutOneDigit = +(leftWithoutCurrent.join(''));
+    if (withoutOneDigit > maxNumber) {
+      maxNumber = withoutOneDigit;
+    }
+  }
+  return maxNumber;
 }
 
 module.exports = {
--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let resultObj = {};
+  
+  for (let i = 0; i < domains.length; i += 1) {
+    let updateDomain = domains[i].split('.').reverse();
+    let subdomain = '';
+    for (let j = 0; j < updateDomain.length; j += 1) {
+      subdomain += '.' + updateDomain[j];
+      if (!resultObj[subdomain]) {
+        resultObj[subdomain] = 1;
+      } else {
+        resultObj[subdomain] += 1;
+      }
+    }
+  }
+  return resultObj;
 }
 
 module.exports = {
